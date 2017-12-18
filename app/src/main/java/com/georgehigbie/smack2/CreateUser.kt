5e114c2430f6de38274dcecbd354168320047390
George Higbie <gehigbie@gmail.com>
2017-12-17 21:26:26 -0700

    Add new activity

---
+++ app/src/main/java/com/georgehigbie/smack2/CreateUser.kt
@@ -0,0 +1,12 @@
+package com.georgehigbie.smack2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateUser : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+}
