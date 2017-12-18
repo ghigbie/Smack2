1dc47bbfa96196fab788ddda7ccdb0c3e90d86b8
George Higbie <gehigbie@gmail.com>
2017-12-17 20:10:07 -0700

    Reformat code

--- app/src/main/java/com/georgehigbie/smack2/LoginActivity.kt
+++ app/src/main/java/com/georgehigbie/smack2/LoginActivity.kt
@@ -2,11 +2,20 @@ package com.georgehigbie.smack2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginButtonClicked(view: View){
+
+    }
+
+    fun loginCreateUserButtonClicked(view: View){
+
     }
 }
