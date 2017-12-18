859cbe8b37f1834d10e484b37d1eb80016abf4b7
George Higbie <gehigbie@gmail.com>
2017-12-17 21:37:13 -0700

    Creat new layout

--- app/src/main/java/com/georgehigbie/smack2/CreateUser.kt
+++ app/src/main/java/com/georgehigbie/smack2/CreateUser.kt
@@ -1,7 +1,7 @@
 package com.georgehigbie.smack2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class CreateUser : AppCompatActivity() {
 
